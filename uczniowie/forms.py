94f330dca7c47531d45e13b9969aeca841ddd07c
Kuba077 <mazurjakub81@gmail.com>
2019-02-22 18:48:16 +0100

    Poprawa bledow aplikacji i dodanie walidacji formularza

--- uczniowie/forms.py
+++ uczniowie/forms.py
@@ -2,24 +2,23 @@
 # quiz-orm/forms.py
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, HiddenField, FieldList
-from wtforms import SelectField, FormField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import SelectField, BooleanField
+from wtforms import StringField, HiddenField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
 
 blad1 = 'To pole jest wymagane'
 
 
 class KlasaForm(FlaskForm):
-    id = HiddenField()
-    nazwa = StringField('Nazwa dodawanej klasy: ', validators=[DataRequired(message=blad1)])
-    rok_naboru = StringField('Rok naboru: ', coerce=int)
-    rok_matury = StringField('Rok matury: ', coerce=int)
-
+  id = HiddenField()
+  nazwa = StringField('Nazwa dodawanej klasy: ', validators=[DataRequired(message=blad1)])
+  rok_naboru = IntegerField('Rok naboru: ', validators=[DataRequired(message=blad1), NumberRange(min=2000, max=100)])
+  rok_matury = IntegerField('Rok matury: ', validators=[DataRequired(message=blad1), NumberRange(min=2000, max=100)])
 
 
 class UczenForm(FlaskForm):
-    id = HiddenField()
-    imie = StringField('Imię  dodawanego ucznia: ', validators=[DataRequired(message=blad1)])
-    nazwisko = StringField('Nazwisko ucznia: ', validators=[DataRequired(message=blad1)])
-    plec = BooleanField('Płeć ucznia: ', validators=[DataRequired(message=blad1)])
-    klasa = SelectField('Klasa', coerce=int)
+  id = HiddenField()
+  imie = StringField('Imię  dodawanego ucznia: ', validators=[DataRequired(message=blad1)])
+  nazwisko = StringField('Nazwisko ucznia: ', validators=[DataRequired(message=blad1)])
+  plec = BooleanField('Płeć ucznia: ', validators=[DataRequired(message=blad1)])
+  klasa = SelectField('Klasa', coerce=int)
